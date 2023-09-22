f2edbe040ac30874cb30f9f0044848c5f0e2bd5b
nhnserg <124461383+nhnserg@users.noreply.github.com>
2023-09-22 18:48:06 +0000

    Deploying to gh-pages from @ nhnserg/goit-js-hw-09@99a16a79f01dfa5cbead97f77901bf34ef0afd37 🚀

--- 01-color-switcher.9e194361.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let o;t.addEventListener("click",(()=>{o||(o=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)),t.disabled=!0})),e.addEventListener("click",(()=>{clearInterval(o),o=null,document.body.style.backgroundColor="white",t.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.9e194361.js.map
