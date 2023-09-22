df0282261fd7a3c7fac6ef95fa19f7b4fe156c23
Serhii <124461383+nhnserg@users.noreply.github.com>
2023-09-22 22:04:46 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -21,11 +21,12 @@ stopBtn.addEventListener('click', () => {
   startBtn.disabled = false;
 });
 
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
 // const startButton = document.querySelector('[data-start]');
 // const stopButton = document.querySelector('[data-stop]');
 // const body = document.body;
