99a16a79f01dfa5cbead97f77901bf34ef0afd37
Serhii <124461383+nhnserg@users.noreply.github.com>
2023-09-22 20:51:21 +0200

    another solution

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -21,8 +21,38 @@ stopBtn.addEventListener('click', () => {
   startBtn.disabled = false;
 });
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+// const startButton = document.querySelector('[data-start]');
+// const stopButton = document.querySelector('[data-stop]');
+// const body = document.body;
+
+// let interval = null;
+
+// function startColorChange() {
+//   startButton.disabled = true;
+//   stopButton.disabled = false;
+
+//   interval = setInterval(() => {
+//     body.style.backgroundColor = getRandomHexColor();
+//   }, 1000);
+// }
+
+// function stopColorChange() {
+//   startButton.disabled = false;
+//   stopButton.disabled = true;
+
+//   clearInterval(interval);
+// }
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+
+// startButton.addEventListener('click', startColorChange);
+// stopButton.addEventListener('click', stopColorChange);
