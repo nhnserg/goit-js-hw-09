185372b3972700ee7465a0c501ed6548e597397b
Serhii <124461383+nhnserg@users.noreply.github.com>
2023-09-23 15:54:51 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -13,28 +13,33 @@ function createPromise(position, delay) {
   });
 }
 
-// Получаем элементы формы один раз
 const form = document.querySelector('.form');
 const delayInput = form.querySelector('input[name="delay"]');
 const stepInput = form.querySelector('input[name="step"]');
 const amountInput = form.querySelector('input[name="amount"]');
 
-// Обработчик события отправки формы
 form.addEventListener('submit', function (e) {
-  e.preventDefault(); // Предотвращаем стандартное поведение формы
+  e.preventDefault();
 
+  // Проверка ввода на корректные числа
   const firstDelay = parseInt(delayInput.value);
   const step = parseInt(stepInput.value);
   const amount = parseInt(amountInput.value);
 
-  // Создаем промисы в цикле
+  if (isNaN(firstDelay) || isNaN(step) || isNaN(amount)) {
+    Notiflix.Notify.Failure('❌ Please enter valid numbers.');
+    return;
+  }
+
+  // Очистка предыдущих уведомлений
+  Notiflix.Notify.closeAll();
+
   for (let i = 0; i < amount; i++) {
     const position = i + 1;
     const delay = firstDelay + i * step;
 
     createPromise(position, delay)
       .then(({ position, delay }) => {
-        // Используем Notiflix для уведомлений
         Notiflix.Notify.Success(
           `✅ Fulfilled promise ${position} in ${delay}ms`
         );
